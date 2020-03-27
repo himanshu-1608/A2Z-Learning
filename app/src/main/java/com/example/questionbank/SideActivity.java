a22eef32a4b5c696096ea212400a07741ba34b84
himanshu-yadav-1608 <ydv1608@gmail.com>
2020-03-27 23:17:52 +0530

    Added side activity,understand intent datapassing

--- app/src/main/java/com/example/questionbank/SideActivity.java
+++ app/src/main/java/com/example/questionbank/SideActivity.java
@@ -10,18 +10,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SideActivity extends AppCompatActivity {
     EditText et1,et2;
     Button b;
     TextView tv3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        et1 = findViewById(R.id.et1);
-        et2 = findViewById(R.id.et2);
-        b   = findViewById(R.id.b1);
-        tv3 = findViewById(R.id.tv3);
+        setContentView(R.layout.activity_side);
+        et1 = (EditText) findViewById(R.id.et1);
+        et2 = (EditText) findViewById(R.id.et2);
+        b   = (Button) findViewById(R.id.b1);
+        tv3 = (TextView) findViewById(R.id.tv3);
 
         Intent aaya = getIntent();
         String one = aaya.getStringExtra("pehla");
@@ -36,18 +36,12 @@ public class MainActivity extends AppCompatActivity {
                 int j = Integer.parseInt(et2.getText().toString());
                 int k = i + j;
                 tv3.setText("Result is : " + k);
-                Toast.makeText(MainActivity.this, "You got answer" + k , Toast.LENGTH_LONG).show();
-                Intent it = new Intent(MainActivity.this, SideActivity.class);
+                Toast.makeText(SideActivity.this, "You got answer" + k , Toast.LENGTH_LONG).show();
+                Intent it = new Intent(SideActivity.this, MainActivity.class);
                 it.putExtra("pehla",et1.getText().toString());
                 it.putExtra("doosra",et2.getText().toString());
                 startActivity(it);
             }
         });
-    }
-    public void doing(View V) {
-        int i = Integer.parseInt(et1.getText().toString());
-        int j = Integer.parseInt(et2.getText().toString());
-        int k = i + j;
-        tv3.setText("Result is : " + k);
     }
 }
