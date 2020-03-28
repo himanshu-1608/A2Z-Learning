fdc0444b625a6a430ece2df6f318ab28447c570e
himanshu-yadav-1608 <ydv1608@gmail.com>
2020-03-29 02:24:09 +0530

    Added a primary signup action

--- app/src/main/java/com/example/questionbank/MainActivity.java
+++ app/src/main/java/com/example/questionbank/MainActivity.java
@@ -10,7 +10,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FILE_NAME = "password.txt";
+
     EditText et1,et2;
     Button b;
     TextView tv3;
@@ -18,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        check();
+
+//        Here starts the actual execution of main Activity . Above this is the code of login activity.
         et1 = findViewById(R.id.et1);
         et2 = findViewById(R.id.et2);
         b   = findViewById(R.id.b1);
@@ -36,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 int j = Integer.parseInt(et2.getText().toString());
                 int k = i + j;
                 tv3.setText("Result is : " + k);
-                Toast.makeText(MainActivity.this, "You got answer" + k , Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Signed up for activity" + k , Toast.LENGTH_LONG).show();
                 Intent it = new Intent(MainActivity.this, SideActivity.class);
                 it.putExtra("pehla",et1.getText().toString());
                 it.putExtra("doosra",et2.getText().toString());
@@ -44,6 +57,62 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void check() {
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            while ((text = br.readLine()) != null) {
+                sb.append(text);
+            }
+            String no = "nanchang";
+            if( sb.toString().equals(no) ){
+                Intent it = new Intent(MainActivity.this , SideActivity.class);
+                it.putExtra("pehla","Signup email");
+                it.putExtra("doosra","Signup Password");
+                startActivity(it);
+            }
+        } catch (FileNotFoundException e) {
+            //e.printStackTrace();
+            setInitial();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void setInitial() {
+        String text = "nanchang";
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            System.out.println("Saved the password for first time : " + text);
+        } catch (IOException io) {
+            io.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException io) {
+                    io.printStackTrace();
+                }
+            }
+            check();
+        }
+    }
+
     public void doing(View V) {
         int i = Integer.parseInt(et1.getText().toString());
         int j = Integer.parseInt(et2.getText().toString());
--- app/src/main/java/com/example/questionbank/SideActivity.java
+++ app/src/main/java/com/example/questionbank/SideActivity.java
@@ -10,9 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class SideActivity extends AppCompatActivity {
+    private static final String FILE_NAME = "password.txt";
     EditText et1,et2;
-    Button b;
+    Button b1,b2;
     TextView tv3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,8 @@ public class SideActivity extends AppCompatActivity {
         setContentView(R.layout.activity_side);
         et1 = (EditText) findViewById(R.id.et1);
         et2 = (EditText) findViewById(R.id.et2);
-        b   = (Button) findViewById(R.id.b1);
+        b1   = (Button) findViewById(R.id.b1);
+        b2   = (Button) findViewById(R.id.b2);
         tv3 = (TextView) findViewById(R.id.tv3);
 
         Intent aaya = getIntent();
@@ -29,7 +34,7 @@ public class SideActivity extends AppCompatActivity {
         et1.setText(one);
         et2.setText(two);
 
-        b.setOnClickListener(new View.OnClickListener() {
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int i = Integer.parseInt(et1.getText().toString());
@@ -41,6 +46,34 @@ public class SideActivity extends AppCompatActivity {
                 it.putExtra("pehla",et1.getText().toString());
                 it.putExtra("doosra",et2.getText().toString());
                 startActivity(it);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String pass = et2.getText().toString();
+            FileOutputStream fos = null;
+            try {
+                fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+                fos.write(pass.getBytes());
+                System.out.println("Saved the password for signup time : " + pass);
+            } catch (IOException io) {
+                io.printStackTrace();
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            }
+
+            Toast.makeText(SideActivity.this, "You got answer", Toast.LENGTH_LONG).show();
+            Intent it = new Intent(SideActivity.this, MainActivity.class);
+            it.putExtra("pehla",et1.getText().toString());
+            it.putExtra("doosra",et2.getText().toString());
+            startActivity(it);
             }
         });
     }
